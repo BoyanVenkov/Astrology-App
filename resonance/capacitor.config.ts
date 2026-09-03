53080c832e563bef5bf0f964f24cea6625edeb1b
Boyan Venkov <boyanvenkov@yahoo.com>
2026-09-03 20:31:39 +0300

    App Uploaded

--- resonance/capacitor.config.ts
+++ resonance/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.resonance.app',
+  appId: 'com.venkovdigital.resonance',
   appName: 'Resonance',
   webDir: 'dist',
   server: {
