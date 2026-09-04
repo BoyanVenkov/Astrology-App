5453953a5a19e8afe255d4783f41c11dcfb63691
Boyan Venkov <boyanvenkov@yahoo.com>
2026-09-04 08:54:55 +0300

    Release Fixes

--- resonance/capacitor.config.ts
+++ resonance/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.venkovdigital.resonance',
+  appId: 'com.resonance.app',
   appName: 'Resonance',
   webDir: 'dist',
   server: {
